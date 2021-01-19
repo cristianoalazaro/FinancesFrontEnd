b7207ec1a7560b8c395dd0136a8652593ab05906
Cristiano Ap Lázaro <cristianoalazaro@hotmail.com>
2021-01-19 19:06:09 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Login from './pages/Login.js';
+
+import './styles/global.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <Login />
+        <div className='global'>
+          <p>sdlkfjsldfjldf</p>
+          <button>Clique aqui</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../styles/login.css';
+
+export default function Login() {
+    return <>
+        <h1 className='title'>Página de Login <small>Oi tudo bem?</small></h1>
+        <p>Loren ipsu,</p>
+        <a href="#">Link</a>
+    </>
+}
